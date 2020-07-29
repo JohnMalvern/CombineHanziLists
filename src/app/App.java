f19bd45f4514b5ee35611a62fe34ca1e87718088
JohnMalvern <john_c_morgan@icloud.com>
2020-07-29 14:29:51 +0800

    Added a comment

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,9 @@ package app;
 
 import java.util.*;
 
+//
+// App to combine hanzi lists sorted by frequency and Heisig number.
+//
 public class App {
     public static void main(final String[] args) throws Exception {
         System.out.println("Hanzi Combiner");
